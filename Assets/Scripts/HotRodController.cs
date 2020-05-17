98efc11dbc7cab2a01c5968fcf37d3881fc0b3bd
rizki <kikirizmau140175@gmail.com>
2020-05-17 18:58:43 +0700

    first commit

---
+++ Assets/Scripts/HotRodController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class HotRodController : MonoBehaviour {
+
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float forward = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+        float rotate = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
+        forward *= Time.deltaTime;
+        rotate *= Time.deltaTime;
+        transform.Translate(0, 0, forward);
+        transform.Rotate(0, rotate, 0);
+		
+	}
+}
---
+++ Assets/Scripts/screenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+	
+
+public class screenController : MonoBehaviour {
+
+    public AudioSource hornSound;
+
+	public void changeLevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void hitHorn()
+    {
+        hornSound.Play();
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
